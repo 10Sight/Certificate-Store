02d4781559cb1771459abba9e1350bb13ad38f62
Jatin Kumar Nagar <jatinnagar563@gmail.com>
2026-06-10 16:36:52 +0530

    URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://certificate-store-server.onrender.com',
         secure: false,
       },
     },
